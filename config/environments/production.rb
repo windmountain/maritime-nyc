0ad3da8a7fd3f5df6c1f8b7126ba5bec74f4b9ca
Zach Rose <zach@windmountainsoftware.com>
2024-05-19 23:45:15 -0400

    Require master key in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "maritime.nyc",      # Allow requests from maritime.nyc
-    /.*\.maritime\.nyc/, # Allow requests from subdomains like `www.example.com`
+    ".maritime.nyc",     # Allow requests from subdomains like `www.example.com`
     "maritimenyc.production"
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
