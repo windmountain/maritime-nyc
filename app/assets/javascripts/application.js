9194f357080e5d5f85c78805096e78de83bf1f16
Zach Rose <zach@windmountainsoftware.com>
2024-06-29 21:50:33 -0400

    Get started on 'blue' theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,13 @@
 //= require calagator
+
+/*
+  Handle ariaHidden/data-expandable
+*/
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('*[data-expandable]').forEach(function(controlElement){
+    controlElement.addEventListener('click', () => {
+      const target = document.getElementById(controlElement.dataset.expandable);
+      target.ariaHidden = target.ariaHidden == "true" ? "false" : "true";
+    });
+  });
+});
