c6596f1d7c9a4a7abafd0ae5f21c9d0e66ec109d
Zach Rose <zach@windmountainsoftware.com>
2024-07-01 15:21:06 -0400

    Add new header with three ways to close

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,27 @@
   Handle ariaHidden/data-expandable
 */
 window.addEventListener('DOMContentLoaded', (event) => {
+
+  document.addEventListener("keydown", function(event) {
+    if(event.key != "Escape") return;
+    const menu = document.getElementById('top_menu');
+    if(menu.ariaHidden == "false") {
+      menu.ariaHidden = "true";
+    }
+  });
+
   document.querySelectorAll('*[data-expandable]').forEach(function(controlElement){
     controlElement.addEventListener('click', () => {
       const target = document.getElementById(controlElement.dataset.expandable);
       target.ariaHidden = target.ariaHidden == "true" ? "false" : "true";
     });
   });
+
+  document.querySelectorAll('*[data-collapsable]').forEach(function(controlElement){
+    controlElement.addEventListener('click', () => {
+      const target = document.getElementById(controlElement.dataset.collapsable);
+      target.ariaHidden = (target.ariaHidden == "true") ? "false" : "true";
+    });
+  });
+
 });
